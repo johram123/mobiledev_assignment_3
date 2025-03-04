591e6061c05e5cecb3fe160e91564dfea0c571c9
ShawnFrancia <159972209+ShawnFrancia@users.noreply.github.com>
2025-03-03 23:05:06 -0700

    create text input to call api
    Added
    *Text input*
    *calling callApi in index*

---
+++ app/index.tsx
@@ -0,0 +1,56 @@
+import { View, Text, TouchableOpacity, Button, StyleSheet, TextInput } from "react-native";
+import { useEffect, useState } from "react";
+import { CallAPI } from "../components/callAPI";
+
+
+export default function App() {
+    const [month, setMonth] = useState<string>("");
+    const [day, setDay] = useState<string>("");
+    
+
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>Assingment 3</Text>
+
+            <CallAPI month={month} day={day} />
+
+            <TextInput style={styles.TextInput} value={month} onChangeText={setMonth} placeholder="Enter Month" /> 
+            <TextInput style={styles.TextInput}value={day} onChangeText={setDay} placeholder="Enter Day" /> 
+        </View> 
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {    
+        flex: 1, 
+        justifyContent: "space-between",
+        alignItems: "center",
+        
+    },
+    header: {
+        alignItems: "center",
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    apiComponent: {
+        alignItems: "center",
+    },
+    button: {
+        padding: 10,
+        width: 120,
+        alignItems: "center",
+        borderRadius: 10,
+        backgroundColor: "#dfe6ef",    
+    },
+    TextInput: {
+        width: 200,
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 10,
+        backgroundColor: "#fff",
+    },
+});
+
---
+++ components/callAPI.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+type DateProps = {
+    day: string;
+    month: string;
+}
+
+const CallAPI = ({month, day} : DateProps) => {
+    const [data, setData] = useState<any>(" ");
+    const [error, setError] = useState<Error | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const url = `https://numbersapi.p.rapidapi.com/${month}/${day}/date`;
+    const options = {
+	    method: 'GET',
+	    headers: {
+		    'x-rapidapi-key': 'f85f5d2000msh989b7f1920d998ep10488djsn2c43dea4b6b8',
+		    'x-rapidapi-host': 'numbersapi.p.rapidapi.com'
+	    }
+    };
+
+    useEffect(() => {
+        makeAPICall();
+    }, [month, day]);
+
+    const makeAPICall = async () => {
+        try
+        {
+            const response = await fetch(url, options);
+            const data = await response.text();
+            console.log(data);
+            setData(data);
+        }
+        catch (error)
+        {
+            setError(error as Error);
+            
+        }
+        finally 
+        {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>{loading && <Text>Loading...</Text>}</Text>
+            <Text>{error && <Text>{error.message}</Text>}</Text>
+            <Text>{data && data}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 200,
+    },
+    item: {
+        marginBottom: 10,
+    },
+});
+
+export {CallAPI};
